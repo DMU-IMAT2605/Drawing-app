13e5b59d7be481b93a5d56de051a189bb59e6205
Stephan <stepan.kolontaj@gmail.com>
2021-11-04 19:09:54 +0000

    Changed Line's class function access properties

--- include/Line.h
+++ include/Line.h
@@ -7,14 +7,13 @@ public:
 	Line() {};
 	Line(sf::Vector2f _x, sf::Vector2f _y);
 	~Line();
-
-	void setPosition(sf::Vector2i _pos);	//unused
-	void changeSize(sf::Vector2f _delta);	//unused
 	
 	void render(sf::RenderTarget& _t);
 
 private:
 	sf::VertexArray vxr;
 
+	void setPosition(sf::Vector2i _pos);	//unused
+	void changeSize(sf::Vector2f _delta);	//unused
 };
 
